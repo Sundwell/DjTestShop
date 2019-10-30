1ad05b87cba0e242b77bf04ed4378f35d0688d79
serhii <lyamtsev.s2017@gmail.com>
2019-10-30 14:28:47 +0200

    improved custom Middleware

--- middleware/product_middleware.py
+++ middleware/product_middleware.py
@@ -3,17 +3,25 @@ from django.utils.deprecation import MiddlewareMixin
 from shop.models import Product
 
 
+class ProductError(Exception):
+    pass
+
+
 class CheckExpired(MiddlewareMixin):
+
     def process_request(self, request):
         # Product.objects.update(expired=False)
         for product in Product.objects.filter(expired=False).values():
             if timezone.localdate() >= product['date_of_expire']:
                 Product.objects.filter(id=product['id']).update(expired=True)
 
-        if Product.objects.last():
-            if Product.objects.last().add_date >= Product.objects.last().date_of_expire:
-                Product.objects.last().delete()
-                raise ValueError(f"You can't add an expired product.")
+        pr_last = Product.objects.last()
+        if pr_last:
+            if pr_last.add_date >= Product.objects.last().date_of_expire:
+                pr_last.delete()
+                raise ProductError(f"You can't add an expired product.")
+
+
 
 
 
