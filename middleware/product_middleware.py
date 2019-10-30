09a336e9fba7a2505bc432f6e01d813d4fd1cc2f
serhii <lyamtsev.s2017@gmail.com>
2019-10-30 13:50:46 +0200

    changed middleware for prod

--- middleware/product_middleware.py
+++ middleware/product_middleware.py
@@ -1,12 +1,23 @@
+from django.utils import timezone
 from django.utils.deprecation import MiddlewareMixin
 from shop.models import Product
 
 
-class AlwaysFreshMiddleware(MiddlewareMixin):
+class CheckExpired(MiddlewareMixin):
+    def process_request(self, request):
+        # Product.objects.update(expired=False)
+        for product in Product.objects.filter(expired=False).values():
+            if timezone.localdate() >= product['date_of_expire']:
+                Product.objects.filter(id=product['id']).update(expired=True)
 
-    def __call__(self, request):
+        if Product.objects.last():
+            if Product.objects.last().add_date >= Product.objects.last().date_of_expire:
+                Product.objects.last().delete()
+                raise ValueError(f"You can't add an expired product.")
 
-        response = self.get_response(request)
-        Product.objects.update(expired=False)
 
-        return response
+
+
+
+
+
