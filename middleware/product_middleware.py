d8b4bbc94b3f6c2bf8b0beb593afd6544a6a0038
serhii <lyamtsev.s2017@gmail.com>
2019-10-30 11:31:40 +0200

    changed middleware for product

--- middleware/product_middleware.py
+++ middleware/product_middleware.py
@@ -1,13 +1,10 @@
+from django.utils.deprecation import MiddlewareMixin
 from shop.models import Product
 
 
-class AlwaysFreshMiddleware:
-    def __init__(self, get_response):
-        self.get_response = get_response
+class AlwaysFreshMiddleware(MiddlewareMixin):
 
     def __call__(self, request):
-
-        response = self.get_response(request)
         Product.objects.update(expired=False)
 
-        return response
+        return super(AlwaysFreshMiddleware, self).__call__()
